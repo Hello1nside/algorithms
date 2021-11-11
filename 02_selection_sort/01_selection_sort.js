f70c441536af3197efc3e55bbcf1e4c9d284c975
Yevhenii Mudrahel <mudragelu@gmail.com>
2021-11-11 17:12:47 +0200

    Selection sort algorithm in JavaScript

---
+++ 02_selection_sort/01_selection_sort.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function findSmallestIndex(array) {
+    let smallestElement = array[0];
+    let smallestIndex = 0;
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < smallestElement) {
+            smallestElement = array[i];
+            smallestIndex = i;
+        }
+    }
+
+    return smallestIndex;
+}
+
+function selectionSort(array) {
+    let sortedArray = [];
+    let length = array.length;
+
+    for (let i = 0; i < length; i++) {
+        // Find the smallest element in the array
+        let smallestIndex = findSmallestIndex(array);
+        // Adds the smallest element to new array
+        sortedArray.push(array.splice(smallestIndex, 1)[0]);
+    }
+
+    return sortedArray;
+}
+
+console.log(selectionSort([5, 3, 6, 2, 9])); // [2, 3, 5, 6, 9]
